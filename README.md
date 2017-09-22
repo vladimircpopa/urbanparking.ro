e4dd4a2496716f3e2c3b6638a3e54ce84f6ac7ca
Vladimir Cristian Popa <cristi_pinky@yahoo.com>
2017-09-22 17:40:54 +0300

    proper 2.8 install

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
+urbanparking.ro
+===============
 
-A Symfony project created on September 22, 2017, 6:17 am.
-=======
-# urbanparking.ro
+A Symfony project created on September 22, 2017, 3:54 pm.
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -15,17 +14,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-    /**
-     * @Route("/login", name="login")
-     */
-    public function loginAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return new Response('asdfasdfasdf weew');
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
     }
 }
